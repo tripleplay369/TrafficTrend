d2eda78d793f4d3678b497519a26e3db4c1f21be
Kelby <green.kelby@gmail.com>
2015-05-26 17:43:03 -0400

    change color of icon/badge

--- traffic.js
+++ traffic.js
@@ -1,9 +1,44 @@
 window.trafficHistory = []
 
+function updateIcon() {
+  var minutesToCheck = 15
+  var threshold = 0.1
+
+  var target = Date.now() - (minutesToCheck * 60 * 1000)
+
+  var closest = null
+  var smallestTimeDist = Number.MAX_VALUE
+
+  var history = window.trafficHistory
+  var newest = history[history.length - 1]
+  for(var i = 0; i < history.length; ++i){
+    var dist = Math.abs(target - history[i].timestamp)
+    if(dist < smallestTimeDist){
+      smallestTimeDist = dist
+      closest = history[i]
+    }
+  }
+
+  var thresholdTime = threshold * newest.trafficTime
+
+  if(newest.trafficTime - closest.trafficTime < -thresholdTime){
+    chrome.browserAction.setIcon({path: "green.png"})
+    chrome.browserAction.setBadgeBackgroundColor({color: '#3eb81d'})
+  }
+  else if(newest.trafficTime - closest.trafficTime > thresholdTime){
+    chrome.browserAction.setIcon({path: "red.png"})
+    chrome.browserAction.setBadgeBackgroundColor({color: '#bf1b1b'})
+  }
+  else{
+    chrome.browserAction.setIcon({path: "yellow.png"})
+    chrome.browserAction.setBadgeBackgroundColor({color: '#e1d736'})
+  }
+}
+
 function run() {
   var interval = 60000
   var retryInterval = 1000
-  var maxHistoryLength = 100
+  var maxHistoryLength = 20
 
   chrome.storage.sync.get(['start', 'end'], function(items){
     if(items['start'] && items['end']){
@@ -25,6 +60,7 @@ function run() {
         window.trafficHistory = window.trafficHistory.slice(-1 * maxHistoryLength)
 
         chrome.browserAction.setBadgeText({text: (time / 60).toFixed(0)})
+        updateIcon()
         setTimeout(run, interval)
       }
       else{
